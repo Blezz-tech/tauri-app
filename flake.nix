19ad24fd1d02e6dc62ea71c310b6af9b44f89ef2
Blezz-tech <72557631+Blezz-tech@users.noreply.github.com>
2024-03-10 19:28:06 +0300

    update

--- flake.nix
+++ flake.nix
@@ -1,57 +1,50 @@
 {
-	description = "LuaLaTeX build env.";
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-	};
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
 
-	outputs = { self, nixpkgs } @inputs: let
-		system = "x86_64-linux";
-		lib    = nixpkgs.lib;
-		pkgs   = nixpkgs.legacyPackages.${system};
-		# tex = (pkgs.texlive.combine {
-		# 	inherit (pkgs.texlive) scheme-basic
-		# 		amsmath
-		# 		babel
-		# 		capt-of
-		# 		catchfile
-		# 		collection-fontsextra
-		# 		cyrillic
-		# 		dvipng
-		# 		dvisvgm
-		# 		environ
-		# 		etoolbox
-		# 		fancyhdr
-		# 		fontspec
-		# 		geometry
-		# 		hyperref
-		# 		listofitems
-		# 		luacode
-		# 		luatexbase
-		# 		montserrat
-		# 		parskip
-		# 		pgf
-		# 		tcolorbox
-		# 		tocloft
-		# 		ulem
-		# 		wrapfig
-		# 		xcolor;
+        libraries = with pkgs;[
+          webkitgtk
+          gtk3
+          cairo
+          gdk-pixbuf
+          glib
+          dbus
+          openssl_3
+          librsvg
+        ];
 
-		# 		#(setq org-latex-compiler "lualatex")
-		# 		#(setq org-preview-latex-default-process 'dvisvgm)
-		# });
-	in {
-		devShells.${system} = {
-			default = pkgs.mkShell rec {
-				nativeBuildInputs = with pkgs; [
-					# gnumake
-					# imagemagick
-					# tex
-					cargo
-					rustc
-				];
-				buildInputs = with pkgs; [];
-			};
-		};
-	};
+        packages = with pkgs; [
+          curl
+          wget
+          pkg-config
+          dbus
+          openssl_3
+          glib
+          gtk3
+          libsoup
+          webkitgtk
+          librsvg
+          cargo
+          rustc
+          nodejs
+        ];
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = packages;
+
+          shellHook =
+            ''
+              export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
+              export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
+            '';
+        };
+      });
 }
